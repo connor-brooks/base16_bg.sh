bc12b75339b144daa8544d43a474beb7416c51c7
c <mail@connor-brooks.com>
2019-02-16 14:57:44 +0000

    Removed pointless whitespace

--- base16_bg.sh
+++ base16_bg.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 convert -size 100x100 xc:#$(cat ~/.base16_theme | grep color00 | head -1 | cut -d '"' -f2 | sed --expression='s/\///g') ~/.base16_bg.png;
 feh --bg-scale ~/.base16_bg.png
-
